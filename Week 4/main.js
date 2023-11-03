abbf14c2be81d6004d13abda21557986db2f6e2e
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2023-11-03 10:04:54 -0700

    changes

--- Week 4/main.js
+++ Week 4/main.js
@@ -1,7 +1,7 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://github.com/ls2092/Web-Programming-Laiba/blob/main/Week%204/cities1.json');
 ourRequest.onload = function() {
-var ourData = ourRequest.responseText;
+var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
 };
 ourRequest.send();
