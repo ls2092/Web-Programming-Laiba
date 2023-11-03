f5b46dee0eadd51097f5ad8c6a101c18f6bc64c1
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2023-11-03 10:00:28 -0700

    chnages

--- Week 4/main.js
+++ Week 4/main.js
@@ -7,24 +7,4 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.send();
 
 
-var myCity = [
-    {
-        "name" : "Dubai",
-        "country" : "UAE",
-        "place" : "Dubai Mall"
-    },
-
-    {
-        "name" : "Milan",
-        "country" : "Italy",
-        "place" : "Domo di Milan"
-    },
-
-    {
-        "name" : "Abu Dhabi",
-        "country" : "UAE",
-        "place" : "Marina Mall"
-    }
-]
-
 console.log(myCity[1].country);
