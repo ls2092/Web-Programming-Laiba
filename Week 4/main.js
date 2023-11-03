1b5aa3aee6259c20c45e34e1ee1c8d0602a51601
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2023-11-03 09:55:55 -0700

    changes to main.js

--- Week 4/main.js
+++ Week 4/main.js
@@ -27,31 +27,3 @@ var myCity = [
     }
 ]
 
-console.log(myCity[1].country);
-
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
-};
-ourRequest.send();
-})
-
-var cityContainer = document.getElementById("city-info");
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-};
-ourRequest.send();
-});
-function renderHTML(data){
-var htmlString = "this is a test";
-cityContainer.insertAdjacentHTML('beforeend' , htmlString);
-}
