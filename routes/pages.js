3e285d8bfcf2ac755b5c797fb132fb9474c1e916
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2023-11-24 09:00:06 -0800

    cahnges

--- routes/pages.js
+++ routes/pages.js
@@ -26,4 +26,16 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
     // Route logic for the profile page for users that are authentic
     res.render('profile', { user: req.user }); //  user information is available in req.user
 });
+
+
+router.get('/profile', (req, res) => {
+   const user = {
+    id: req.query.id,
+    name: req.query.name,
+    email: req.query.email
+   };
+    
+    res.render('profile', { user}); 
+});
+
 module.exports = router;
